23746d58067a411c3075bb437c1d834717b8ec12
Isidro A. López G <me@ialopezg.com>
2020-09-05 00:08:35 -0500

    Added general error template.

---
+++ App/Views/errors/general.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Error</title>
+    <link rel="stylesheet" href="http://basecss.local/dist/css/basecss.css">
+</head>
+    <body>
+        <div class="card">
+            <header class="container bg-lightgrey">
+                <h3><?= $heading ?></h3>
+            </header>
+            <div class="container">
+                <p><?= $message ?></p>
+            </div>
+        </div>
+    </body>
+</html>
